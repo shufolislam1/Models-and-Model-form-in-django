6cdee14ba4ce8e44c0ffee86864ce79d86e62e3a
shufolislam1 <shufolislam1255@gmail.com>
2023-12-11 18:13:50 +0600

    one video done

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(primary_key=True)
+    address = models.TextField()
+    father_name = models.TextField(default="janina")
+    
+    
